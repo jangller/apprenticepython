1d0217e0d456b02540e96fd6025772e530cb57cf
jangller <85588069+jangller@users.noreply.github.com>
2021-07-19 23:19:06 -0300

    Add files via upload

---
+++ c.py
@@ -0,0 +1,33 @@
+#troca a cor das palavras
+black = '\033[30m'
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+purple = '\033[35m'
+cyan = '\033[36m'
+grey = '\033[37m'
+white = '\033[97m'
+
+#troca a cor que está em volta das palavras
+ablack = '\033[40m'
+ared = '\033[41m'
+agreen = '\033[42m'
+ayellow = '\033[43m'
+ablue = '\033[44m'
+apurple = '\033[45m'
+acyan = '\033[46m'
+agrey = '\033[47m'
+awhite = '\033[107m'
+
+#Deixar palavras em negrito
+bold = '\033[1m'
+
+#Deixar as palavras sublinhadas
+under = '\033[4m'
+
+#Deixar as cores invertidas
+rever = '\033[7m'
+
+#deixar o padrão do python
+clean = '\033[m'
